927cc3730b3ad5fb07c4241fe11d8a4ef83f3732
cedrik8200 <cedrik8200@cedrik8200-Nitro-AN515-42>
2021-10-28 16:54:11 +0100

    modification des requetes JS

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-
-AddElementCategoryBest();
-
 function getBestTitles(movie, el) {
 	var bestTitle = document.createElement("h1");
 	console.log("bestTitle",movie)
@@ -53,48 +50,104 @@ function AddElementBest() {
 AddElementBest();
 
 function AddElementCategoryBest() {
-	var movies = document.getElementById("btnBest");
+	var movies = document.getElementById("aff");
 
-	fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=9&imdb_score_max=9.7&title=&title_contains=&genre=&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=")
+	fetch("http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=9.4&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=")
 		.then((res) => res.json())
 		.then((data) => {
 			const { results } = data;
 			const categoryBest = results.map((movie, index) => {
 
-				var categoryImage = document.createElement("div");
-				var id = `box${index}`;
+				var categoryImage = document.createElement("a");
+				categoryImage.setAttribute("id", `btnBest${index}`);
 				categoryImage.setAttribute("class", "boxing");
-					getCategoryBestImage(movie, categoryImage, id);
-					myModal(movie.url , id);
-				return categoryImage;
+					getCategoryBestImage(movie, categoryImage, index);
+					getElementDivModal(categoryImage, index);
+				movies.appendChild(categoryImage);
+				categoryImage.addEventListener("click", myModal(movie.url, index));
 			});
 			categoryBest.forEach((movie) => movies.appendChild(movie));
 		});
 }
 
-function getCategoryBestImage(movie, el, id) {
+AddElementCategoryBest();
+
+function getCategoryBestImage(movie, el, index) {
 	var bestImage = document.createElement("img");
-	bestImage.setAttribute("id", id);
+	bestImage.setAttribute("id", `box${index}`);
 	const image = movie.image_url;
 	bestImage.setAttribute("src", image);
 	el.appendChild(bestImage);	
 }
 
-function myModal(movie, id) {
-	var modal = document.getElementById("myModal");
+// element creer pour la modal 
+function getElementDivModal(el, index) {
+	var elementDiv = document.createElement("div");
+	elementDiv.setAttribute("id", `myModal${index}`);
+	elementDiv.setAttribute("class", "boxing_movies modal");
+		getElementDiv2Modal(elementDiv, index);
+	el.appendChild(elementDiv);
+}
+function getElementDiv2Modal(el, index) {
+	var elementDiv2 = document.createElement("div");
+	elementDiv2.setAttribute("class", "modal-content");
+		getElementDiv3Modal(elementDiv2);
+		getElementDiv4Modal(elementDiv2, index);
+		getElementDiv5Modal(elementDiv2);
+	el.appendChild(elementDiv2);
+}
+function getElementDiv3Modal(el) {
+	var elementDiv3 = document.createElement("div");
+	elementDiv3.setAttribute("class", "modal-header");
+		getElementSpanModal(elementDiv3);
+		getElementH2Modal(elementDiv3)
+	el.appendChild(elementDiv3);
+}
+function getElementSpanModal(el) {
+	var elementSpan = document.createElement("span");
+	elementSpan.setAttribute("class", "close");
+	elementSpan.innerHTML = "&times";
+	el.appendChild(elementSpan);
+}
+function getElementH2Modal(el) {
+	var elementH2 = document.createElement("h2");
+	elementH2.innerText = "JustStreamIt";
+	el.appendChild(elementH2);
+}
+function getElementDiv4Modal(el, index) {
+	var elementDiv4 = document.createElement("div");
+	elementDiv4.setAttribute("id", `corpsModal${index}`);
+	elementDiv4.setAttribute("class", "modal-body");
+	el.appendChild(elementDiv4);
+}
+function getElementDiv5Modal(el) {
+	var elementDiv5 = document.createElement("div");
+	elementDiv5.setAttribute("class", "modal-footer");
+		getElementH4Modal(elementDiv5);
+	el.appendChild(elementDiv5);
+}
+function getElementH4Modal(el) {
+	var elementH4 = document.createElement("h4");
+	elementH4.innerText = "Projet6 Openclassrooms";
+	el.appendChild(elementH4);
+}
 
-	var onBouton = document.getElementById("btnBest");
-	console.log("id", id);
+// modal
+function myModal(movie, id) {
+	
+	var modal = document.getElementById(`myModal${id}`);
+
+	var onBouton = document.getElementById(`btnBest${id}`);
+	
 	var span = document.getElementsByClassName("close") [0];
 
 	onBouton.onclick = function() {
 		modal.style.display = "block";
+		dataModal(movie, id);
 	}
 	span.onclick = function() {
 		modal.style.display = "none";
 	}
-
-	dataModal(movie);
 
 	window.onclick = function(event) {
 		if (event.target == modal) {
@@ -118,20 +171,21 @@ function getAddYear(movie , el){
 
 function getAddPic(movie, el) {
 	var addPic = document.createElement("img");
+	addPic.setAttribute("class", "imageModal");
 	const image = movie.image_url;
 	addPic.setAttribute("src", image);
 	el.appendChild(addPic);
 }
 
 
-function dataModal(data) {
-	var surveillance = document.getElementById("corpsModal");
+function dataModal(data, index) {
+	var surveillance = document.getElementById(`corpsModal${index}`);
 
 	fetch(data)
 		.then((res) => res.json())
 		.then((movie) => {
 		var elementsDiv = document.createElement("div");
-		elementsDiv.setAttribute("id", "essaiDiv");
+		elementsDiv.setAttribute("class", "DivModal");
 			getAddTitle(movie, elementsDiv);
 			getAddYear(movie, elementsDiv);
 			getAddPic(movie, elementsDiv);
